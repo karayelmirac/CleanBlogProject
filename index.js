1143676e38da9275fe5c4f541ac27c30b3c25a35
Mirac Karayel <mirackarayel07@gmail.com>
2023-05-07 21:05:15 +0300

    Workspace

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const server = express()
+
+server.get('/',(req,res)=> {
+    const blog = { id: 1, title: "Blog title", description: "Blog description" } 
+    res.send(blog)
+})
+
+
+const port = 5000
+server.listen(port,()=> {
+    console.log(`Server is running ${port}`)
+})
