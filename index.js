6a29112955e3952212085193c1e2d5d0f503e737
Mirac Karayel <mirackarayel07@gmail.com>
2023-05-14 00:47:30 +0300

    Change something for template engine

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express')
-
+const ejs = require('ejs')
 const server = express()
 
-server.get('/',(req,res)=> {
+server.set("view engine","ejs")
+/* server.get('/',(req,res)=> {
     const blog = { id: 1, title: "Blog title 1", description: "Blog description" } 
     res.send(blog)
+}) */
+
+server.use(express.static('public'));
+
+
+//ROUTES
+
+server.get('/',(req,res)=> {
+    res.render('index')
+})
+server.get('/post',(req,res)=> {
+    res.render('post')
+})
+server.get('/about',(req,res)=> {
+    res.render('about')
+})
+server.get('/add_post',(req,res)=> {
+    res.render('add_post')
 })
 
 
