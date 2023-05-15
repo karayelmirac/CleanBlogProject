91c744c831a0efff1f69a914dfa490731bcbecc8
Mirac Karayel <mirackarayel07@gmail.com>
2023-05-15 21:10:12 +0300

    Initilization dynamic content

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const ejs = require('ejs')
 const server = express()
+const mongoose = require('mongoose');
+const Post = require('./models/Post')
 
 server.set("view engine","ejs")
 /* server.get('/',(req,res)=> {
@@ -9,12 +11,19 @@ server.set("view engine","ejs")
 }) */
 
 server.use(express.static('public'));
+server.use(express.urlencoded({extended:true}))
+server.use(express.json())
 
+//connect DB
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 //ROUTES
 
-server.get('/',(req,res)=> {
-    res.render('index')
+server.get('/',async (req,res)=> {
+    const posts = await Post.find({})
+    res.render('index',{
+      posts
+    });
 })
 server.get('/post',(req,res)=> {
     res.render('post')
@@ -24,6 +33,11 @@ server.get('/about',(req,res)=> {
 })
 server.get('/add_post',(req,res)=> {
     res.render('add_post')
+})
+
+server.post('/posts',async (req,res)=> {
+    await Post.create(req.body)
+    res.redirect('/')
 })
 
 
